dbb732b544982eb58d66e338c9aa0725aaebc57a
eliasjunniior <93531081+eliasjunniior@users.noreply.github.com>
2024-03-10 20:53:50 -0300

    Questão2.js

--- Questão2.js
+++ Questão2.js
@@ -1,1 +1,15 @@
+const readline = require('readline-sync');
 
+function lerNumero() {
+  const numero = readline.question('Digite um número: ');
+  return parseFloat(numero);
+}
+
+const num1 = lerNumero();
+const num2 = lerNumero();
+const num3 = lerNumero();
+
+const soma = num1 + num2 + num3;
+const media = soma / 3;
+
+console.log(`A média dos números ${num1}, ${num2} e ${num3} é ${media}`);
