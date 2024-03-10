407d2d085c9e7f42857ed51432721879a61f223f
eliasjunniior <93531081+eliasjunniior@users.noreply.github.com>
2024-03-10 18:06:49 -0300

    Add files via upload

---
+++ Aula1_Java.js
@@ -0,0 +1,13 @@
+let prompt = require('prompt-sync')();
+
+let v1;
+v1 = Number(prompt("Digite o primeiro número:"));
+
+var v2;
+v2 = Number(prompt("Digite o segundo número:"));
+
+var v3;
+v3 = Number(prompt("Digite o terceiro número:"));
+
+alert("Os números digitados foram: " + v1 + ", " + v2 + " e " + v3);
+
